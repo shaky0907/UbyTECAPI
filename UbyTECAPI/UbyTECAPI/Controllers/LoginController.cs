ac306e85c23c293899b0a62bf8245b11c8fd193d
shaky0907 <divad0907@gmail.com>
2022-11-23 04:20:39 -0600

    login controller

---
+++ UbyTECAPI/UbyTECAPI/Controllers/LoginController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Npgsql;
+using System.Data;
+using UbyTECAPI.Models;
+
+namespace UbyTECAPI.Controllers
+{
+    [Route("login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private DataTable execquery(string query)
+        {
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("UbyTecCon");
+
+            NpgsqlDataReader myReader;
+
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return table;
+
+        }
+
+        private string get_idA(string id_admin)
+        {
+            string query2 = @"select cedula_j as ""ID"" from afiliado where cedula_a = '" + id_admin + "';";
+            DataTable table2 = execquery(query2);
+            string json2 = JsonConvert.SerializeObject(table2);
+            List<Afiliado> af = JsonConvert.DeserializeObject<List<Afiliado>>(json2);
+            try
+            {
+                return af[0].ID;
+            }
+            catch (Exception)
+            {
+                return "";
+                throw;
+            }
+           
+        }
+
+
+
+
+        
+
+
+
+        [HttpGet]
+        [Route("get/{username}/{password}")]
+        public string Get(string username, string password)
+        {
+            string query = @"select usuario as ""Username"",contra as ""Password""
+                             from empleado
+                             where usuario = '" + username + "'AND contra = '" + password + "'";
+
+            DataTable table1 = execquery(query);
+            
+
+            string query2 = @"select usuario as ""Username"",contra as ""Password"",cedula as ""ID""
+                             from cliente
+                             where usuario = '" + username + "' AND contra = '" + password + "'";
+
+            DataTable table2 = execquery(query2);
+            
+
+
+            string query3 = @"select usuario as ""Username"",contra as ""Password"",cedula as ""ID""
+                             from admin_afiliado
+                             where usuario = '" + username + "'AND contra = '" + password + "'";
+
+            DataTable table3 = execquery(query3);
+
+
+
+
+
+            if (table1.Rows.Count > 0)
+            {
+                Login login = new Login();
+                login.Type = "admin";
+
+                
+                var jsonC = JsonConvert.SerializeObject(login, Formatting.Indented);
+                return jsonC;
+
+            }
+
+            else if (table2.Rows.Count > 0)
+            {
+                string json = JsonConvert.SerializeObject(table2);
+                List<Cliente> af = JsonConvert.DeserializeObject<List<Cliente>>(json);
+
+                Login login = new Login();
+                login.Type = "cliente";
+                login.ID_client = af[0].ID;
+                
+
+                var jsonC = JsonConvert.SerializeObject(login, Formatting.Indented);
+                return jsonC;
+
+            }
+
+            else if (table3.Rows.Count > 0)
+            {
+                string json = JsonConvert.SerializeObject(table3);
+                List<AdminAfiliado> af = JsonConvert.DeserializeObject<List<AdminAfiliado>>(json);
+
+
+
+                Login login = new Login();
+                login.Type = "affiliate";
+                login.ID_Affiliate = get_idA(af[0].ID);
+                login.ID_Admin = af[0].ID;
+
+                var jsonC = JsonConvert.SerializeObject(login, Formatting.Indented);
+                return jsonC;
+            }
+            else
+            {
+                Login login = new Login();
+                var jsonC = JsonConvert.SerializeObject(login, Formatting.Indented);
+                return jsonC;
+            }
+
+         
+
+        }
+
+
+
+
+
+    }
+}
---
+++ UbyTECAPI/UbyTECAPI/Models/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace UbyTECAPI.Models
+{
+    public class Login
+    {
+
+        public string Type { get; set; }
+        public string ID_client { get; set; }
+        public string ID_Affiliate { get; set; }
+        public string ID_Admin { get; set;}
+    }
+}
