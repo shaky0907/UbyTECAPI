baaa63ce3d65d4a13d0c8ff0728f20d1193b3010
shaky0907 <divad0907@gmail.com>
2022-11-23 23:51:49 -0600

    query fix

--- UbyTECAPI/UbyTECAPI/Controllers/PedidoController.cs
+++ UbyTECAPI/UbyTECAPI/Controllers/PedidoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Npgsql;
+using System.Collections.Generic;
 using System.Data;
 using UbyTECAPI.Models;
 
@@ -342,6 +343,132 @@ namespace UbyTECAPI.Controllers
             return jsonC;
 
         }
+
+
+        [HttpGet]
+        [Route("get-af/{id}")]
+        public string Getaf(string id)
+        {
+            string query = @"select ca.num_carrito as ""ID"", ca.cedula_c as ""ClienteID"", 
+                            C.nombre as ""ClienteN"",C.apellido1 as ""ClienteLN"",
+                            C.provincia as ""Province"", C.canton as ""Canton"", C.distrito as ""District""
+                            from carrito as Ca,cliente as C
+                            where Ca.cedula_c = C.cedula";
+
+            DataTable table = execquery(query);
+
+            string json = JsonConvert.SerializeObject(table);
+            List<OrderView> orders = JsonConvert.DeserializeObject<List<OrderView>>(json);
+            List<OrderView> erase = new List<OrderView>();
+
+
+            foreach (OrderView order in orders)
+            {
+
+                query = @"select ""ID"",""Name"",""Price"",""Cantidad"",""Cedula_a""
+                          from productoview
+                          where ""ID_Carrito"" = " + order.ID + "";
+
+                DataTable pxctable = execquery(query);
+                string jsonpxc = JsonConvert.SerializeObject(pxctable);
+                List<ProductoView> pxc = JsonConvert.DeserializeObject<List<ProductoView>>(jsonpxc);
+
+                
+                string[] Names = new string[pxc.Count];
+                string[] Prices = new string[pxc.Count];
+                string[] Quant = new string[pxc.Count];
+                string[] Ids = new string[pxc.Count];
+                int i = 0;
+                int price = 0;
+                string cedula_a = "";
+                foreach (ProductoView prod in pxc)
+                {
+                    Names[i] = prod.Name;
+                    Prices[i] = prod.Price;
+                    Quant[i] = prod.Cantidad;
+                    Ids[i] = prod.ID;
+                    price += Int32.Parse(prod.Price) * Int32.Parse(prod.Cantidad);
+
+                    cedula_a = prod.Cedula_a;
+                    i++;
+                }
+
+                order.Products = Names;
+                order.ProductPrices = Prices;
+                order.ProductQuantities = Quant;
+                order.ProductIDs = Ids;
+
+
+                order.Price = price.ToString();
+
+                query = @"select cedula_j as ""ID"", nombre as ""Name""
+                          from afiliado
+                          where cedula_j = '" + cedula_a + "';";
+
+                DataTable atable = execquery(query);
+                string jsonaf = JsonConvert.SerializeObject(atable);
+                List<Afiliado> af = JsonConvert.DeserializeObject<List<Afiliado>>(jsonaf);
+
+                try
+                {
+                    if (af[0].ID != id)
+                    {
+                        erase.Add(order);
+                    }
+                    else
+                    {
+                        order.RestID = af[0].ID;
+                        order.RestName = af[0].Name;
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    order.RestID = "";
+                    order.RestName = "";
+                }
+
+                query = @"select estado as ""Estado""
+                          from pedido
+                          where id_pedido = " + order.ID;
+
+
+                DataTable ptable = execquery(query);
+
+                if (ptable.Rows.Count > 0)
+                {
+                    string jsonP = JsonConvert.SerializeObject(ptable);
+                    List<Pedido> pedidos = JsonConvert.DeserializeObject<List<Pedido>>(jsonP);
+                    
+                    order.Status = pedidos[0].Estado;
+                    
+                    
+                }
+                else
+                {
+                    erase.Add(order);
+                    order.Status = "pending";
+                }
+
+
+
+
+            }
+
+            foreach (OrderView e in erase)
+            {
+                orders.Remove(e);
+            }
+
+
+            var jsonC = JsonConvert.SerializeObject(orders, Formatting.Indented);
+
+            return jsonC;
+
+        }
+
+
+
 
 
         [HttpGet]
